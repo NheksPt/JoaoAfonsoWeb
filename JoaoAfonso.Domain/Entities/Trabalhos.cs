7185a2df1894c468d126e19926f95acd40463bf7
Moria <Moria@DESKTOP-8AVSGTE>
2023-10-17 23:29:00 +0100

    17/10/2023 23:27

---
+++ JoaoAfonso.Domain/Entities/Trabalhos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoaoAfonso.Domain.Entities
+{
+    public class Trabalhos
+    {
+        public int ID { get; set; }
+        public required string Nome { get; set; }
+        public string? Descricao { get; set; }
+        public required string UrlImagem { get; set; }
+    }
+}
