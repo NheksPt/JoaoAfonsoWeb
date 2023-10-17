aac55307498014a32bd3391c1a2ea7d112ac5139
Moria <Moria@DESKTOP-8AVSGTE>
2023-10-17 19:43:29 +0100

    Add project files.

---
+++ JoaoAfonsoWeb/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using JoaoAfonsoWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace JoaoAfonsoWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Modelacao()
+        {
+            return View();
+        }
+
+        public IActionResult Animacao()
+        {
+            return View();
+        }
+
+        public IActionResult Escultura()
+        {
+            return View();
+        }
+
+        public IActionResult Contactos()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
